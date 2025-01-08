d8c1a621ef1fd29c698d4ea310af3315fa6ac1a3
PauloDrefDownloading <pmdrefahl5251@eagle.fgcu.edu>
2025-01-07 20:30:14 -0500

    Init commit

---
+++ ArquiveMed/ArquiveMedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArquiveMedApp.swift
+//  ArquiveMed
+//
+//  Created by Paulo Drefahl on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ArquiveMedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
